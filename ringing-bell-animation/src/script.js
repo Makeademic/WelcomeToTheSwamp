bc4859c56fb0976a4aa4c0b846caffaa396711ca
Thomas Storey <thomas@feathr.co>
2020-05-30 22:14:29 -0400

    fix some visual bugs on welcome and bells page

--- ringing-bell-animation/src/script.js
+++ ringing-bell-animation/src/script.js
@@ -5,9 +5,18 @@ var hammer = new Hammer(bell);
 
 hammer.on('tap', ringBell);
 
-
 var audio = document.getElementById("CenturyTowerBells"); 
-
-function ringBell(){
-audio.play()
+// add an event listener to the audio element so that when
+// it finishes playing, the class on the bell resets.
+audio.onended = function () {
+  bell.classList = 'bell';
+};
+function ringBell() {
+  // if the audio is already playing, don't do anything
+  if (!audio.paused) {
+    return;
+  }
+  // add the `ringing` class to the bell so it starts ringing
+  bell.classList = 'bell ringing';
+  audio.play();
 }
--- welcome-to-the-swamp-digitization/src/script.js
+++ welcome-to-the-swamp-digitization/src/script.js
@@ -2,33 +2,68 @@ var currentPage = 0;
 // Get a reference to an element.
 var book = document.querySelector('.book');
 
-// Create an instance of Hammer with the reference.
-var hammer = new Hammer(book);
-
-/*
 $('.book')
 .on('click', '.active', nextPage)
 .on('click', '.flipped', prevPage);
-*/
 
-//For all instances of hammer, which calls book, on the touch event, execute function
+// Create an instance of Hammer with the reference.
+var hammer = new Hammer(book);
+// For all instances of hammer, which calls book, on the touch event, execute function
 // Options include panleft, panright, tap, press, doubletap, pinch, rotate, swipeleft, swiperight
 hammer.on('swipeleft', nextPage);
 hammer.on('swiperight', prevPage);
 
+var timeout;
+
 function prevPage() {
+  // if a page flip is already in process, interrupt the ongoing
+  // timeout so it does not hide pages it should not hide
+  if (timeout) {
+    clearTimeout(timeout);
+    timeout = undefined;
+  }
+  // set previous page to be active
   $('.flipped')
-    .last()
     .removeClass('flipped')
     .addClass('active')
     .siblings('.page')
     .removeClass('active');
+  // show page hidden behind old flipped page
+  $('.prevFlipped')
+    .last()
+    .addClass('flipped')
+    .removeClass('prevFlipped')
+    .removeClass('hidden');
 }
 function nextPage() {
+  // if there are no more pages - don't do anything!
+  if ($('.active').length === 0) {
+    return;
+  }
+  // if a page flip is in progress, interrupt the timeout 
+  // so we don't hide pages that shouldn't be hidden
+  if (timeout) {
+    clearTimeout(timeout);
+    timeout = undefined;
+  }
+  // current flipped page needs to be hidden behind next flipped page
+  $('.flipped')
+    .addClass('prevFlipped')
+    .removeClass('flipped');
+  // current active page becomes flipped, and its next sibling becomes
+  // new active page
   $('.active')
     .removeClass('active')
     .addClass('flipped')
     .next('.page')
-    .addClass('active')
-    .siblings();
+    .addClass('active');
+  // this is the hacky bit - for some reason firefox on ipad was glitching
+  // out and showing the "prevFlipped" page (the page "under" the flipped page)
+  // for one frame just as the css transition ended.
+  // This adds a timeout so that a few milliseconds before that happens, we hide
+  // the "prevFlipped" page so there's no flicker
+  timeout = setTimeout(function () {
+    $('.prevFlipped').addClass('hidden');
+    timeout = undefined;
+  }, 1400);
 }
