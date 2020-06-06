3e352cf402411e14a3e786e121c7ceaaebdf9db8
Makeademic <kaplanwizard@gmail.com>
2020-06-05 21:42:30 -0400

    changed href

--- ringing-bell-animation/dist/script.js
+++ ringing-bell-animation/dist/script.js
@@ -1,13 +1,26 @@
-var bell = document.querySelector('.bell');
+var bell = document.querySelector(".bell");
 
 // Create an instance of Hammer with the reference.
 var hammer = new Hammer(bell);
 
-hammer.on('tap', ringBell);
+hammer.on("tap", ringBell);
 
+var audio = document.getElementById("CenturyTowerBells");
+// add an event listener to the audio element so that when
+// it finishes playing, the class on the bell resets.
+audio.onended = function () {
+  bell.classList = "bell";
+};
+function ringBell() {
+  // if the audio is already playing, don't do anything
+  if (!audio.paused) {
+    return;
+  }
+  // add the `ringing` class to the bell so it starts ringing
+  bell.classList = "bell ringing";
+  audio.play();
+}
 
-var audio = document.getElementById("CenturyTowerBells"); 
-
-function ringBell(){
-audio.play()
+function goBack() {
+  window.history.back();
 }
